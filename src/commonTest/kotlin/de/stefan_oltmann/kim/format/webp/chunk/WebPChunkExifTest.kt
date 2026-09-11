9c33311e27dcb6af9a03b43f2ee52add219de841
Stefan Oltmann <github@stefan-oltmann.de>
2026-09-11 08:32:44 +0200

    Read WebP EXIF chunks with the improper Exif header
    Some writers add the JPEG style Exif header although the WebP
    specification defines the chunk as raw TIFF bytes. ExifTool warns
    'Improper EXIF header' for those and still reads them; Kim failed the
    whole file. The header is now skipped like ExifTool does.

---
+++ src/commonTest/kotlin/de/stefan_oltmann/kim/format/webp/chunk/WebPChunkExifTest.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2026 Stefan Oltmann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.stefan_oltmann.kim.format.webp.chunk
+
+import de.stefan_oltmann.kim.testdata.KimTestData
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+/**
+ * Tests reading the EXIF chunk of WebP files.
+ *
+ * The chunk carries raw TIFF bytes per the WebP specification, but some
+ * writers add the JPEG style "Exif\0\0" header in front of it. ExifTool
+ * warns "Improper EXIF header" for those and still reads them.
+ */
+class WebPChunkExifTest {
+
+    private val tiffBytes: ByteArray
+        get() = KimTestData.getHeaderExifBytesOf(52)
+
+    @Test
+    fun testReadsExifWithoutHeader() {
+
+        val chunk = WebPChunkExif(tiffBytes)
+
+        assertNotNull(chunk.tiffContents)
+    }
+
+    @Test
+    fun testReadsExifWithImproperHeader() {
+
+        val chunk = WebPChunkExif(
+            "Exif\u0000\u0000".encodeToByteArray() + tiffBytes
+        )
+
+        val chunkWithoutHeader = WebPChunkExif(tiffBytes)
+
+        /*
+         * The toString covers the whole parsed structure - TiffContents
+         * equality compares the embedded thumbnail bytes by reference.
+         */
+        assertEquals(
+            expected = chunkWithoutHeader.tiffContents.toString(),
+            actual = chunk.tiffContents.toString()
+        )
+
+        assertEquals(
+            expected = chunkWithoutHeader.tiffContents.directories.size,
+            actual = chunk.tiffContents.directories.size
+        )
+    }
+}
